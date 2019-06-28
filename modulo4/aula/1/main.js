5d45364ddba0f1ac091607cadd165846aae612bd
unknown <nathalia_a_silva@saojose.sc.senai.br>
2019-06-28 08:44:43 -0300

    fazer com que apareça o avatar utilizando o response

--- modulo4/aula/1/main.js
+++ modulo4/aula/1/main.js
@@ -1,5 +1,5 @@
 api.get('https://api.github.com/users/aoskfaposf')
     .then(function(response) {
-        console.log(response);
+        console.log(response.data.avatar_url);
     })
     .catch(function(error) {})
