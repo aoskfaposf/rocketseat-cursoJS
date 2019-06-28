ea4590679c4953ff85b263c5b950ae2a372487d1
unknown <nathalia_a_silva@saojose.sc.senai.br>
2019-06-28 08:41:14 -0300

    removendo promise

--- modulo4/aula/1/main.js
+++ modulo4/aula/1/main.js
@@ -1,21 +1,3 @@
-var promises = function() {
-    return new Promise(function(resolve, reject) {
-    var xhr = new XMLHttpRequest();
-    xhr.open('https://api.github.com/users/aoskfaposf');
-    xhr.send(null);
-
-    xhr.onreadystatechange = function () {
-        if(xhr.readyState === 4) {
-        if(xhr.status === 200){
-         resolve(JSON.parse(xhr.responseText)); 
-        }else
-        reject('Erro na requisição');
-     }
-    }
-  }
- });
-}
-
 minhaPromise()
     .then(function(response) {
         console.log(response);
