ac4fea3e33cac8b4b8fd05840b962fdc2ae70b9c
unknown <nathalia_a_silva@saojose.sc.senai.br>
2019-06-28 08:16:12 -0300

    colocando parâmetros

--- modulo4/aula/1/main.js
+++ modulo4/aula/1/main.js
@@ -2,5 +2,6 @@ var promises = function() {
     return new Promise(function(resolve, reject) {
     var xhr = new XMLHttpRequest();
     xhr.open('https://api.github.com/users/aoskfaposf');
+    xhr.send(null);
     });
 }
