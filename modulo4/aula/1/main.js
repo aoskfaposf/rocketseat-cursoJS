b1222b1671b6d5d7118438eb8596dfe13f38efef
unknown <nathalia_a_silva@saojose.sc.senai.br>
2019-06-28 08:09:36 -0300

    adicionando uma variável e uma função promises

--- modulo4/aula/1/main.js
+++ modulo4/aula/1/main.js
@@ -1,10 +1,3 @@
-var xhr = new XMLHttpRequest();
-
-xhr.open('GET', 'https://api.github.com/users/aoskfaposf')
-xhr.send(null);
-
-xhr.onreadystatechange = function() {
-    if (xhr.readyState === 4) {
-        console.log(JSON.parse(xhr.responseText));
-    }
+var promises = function() {
+    
 }
